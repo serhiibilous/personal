77507f691b109a2765098af83025d932c2ee93f3
Serhii Bilous <serhiybilouss@gmail.com>
2023-03-05 18:56:21 -0500

    Updated colors for header navigation

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -119,7 +119,7 @@ function NavItem({ href, children }: any) {
         className={clsx(
           "relative block px-3 py-2 transition whitespace-nowrap",
           isActive
-            ? "text-teal-500 dark:text-teal-400"
+            ? "text-teal-700 dark:text-teal-400"
             : "hover:text-teal-500 dark:hover:text-teal-400"
         )}
       >
